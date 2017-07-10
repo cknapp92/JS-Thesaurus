184931a8b53f01c61a9e7e68be0de47f8cd3aeb6
Conrad Knapp <conrad.knapp@bc.edu>
2017-07-10 19:20:50 -0400

    add whatShould file

---
+++ whatShouldIWear.js
@@ -0,0 +1,14 @@
+function whatShouldIWear(temp) {
+	if (temp < 60) {
+		console.log("Wear a jacket");
+} else if (temp < 70) {
+		console.log("Wear a sweater");
+} else {
+		console.log("Wear a t-shirt");
+}
+	
+}
+
+whatShouldIWear(50);
+whatShouldIWear(60);
+whatShouldIWear(80);
