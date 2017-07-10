a3252e4bfb0b13361634b6466ff4452f406fb0a2
Conrad Knapp <conrad.knapp@bc.edu>
2017-07-10 18:58:03 -0400

    add dogs.js

---
+++ dogs.js
@@ -0,0 +1,12 @@
+function bark(name, weight) {
+	if (weight > 20) {
+		console.log(name + " says WOOF WOOF");
+	} else {
+		console.log(name + " says woof woof");
+	}
+}
+
+bark("fido", 23);
+bark("spike", 19);
+bark("spot", 21);
+bark("rex", 17);
