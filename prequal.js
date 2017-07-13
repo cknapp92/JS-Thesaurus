7b8e58a266ca3700c90da16f13c9d2bcddf6983f
Conrad Knapp <conrad.knapp@bc.edu>
2017-07-13 19:54:14 -0400

    create secret code files

--- prequal.js
+++ prequal.js
@@ -9,6 +9,36 @@ var taxi = {
 
 };
 
+var cadi = {
+	make: "GM",
+	model: "Cadillac",
+	year: 1955, 
+	color: "tan",
+	passengers: 5,
+	convertible: false,
+	mileage: 12892
+}
+
+var fiat = {
+	make: "Fiat",
+	model: "500",
+	year: 1957, 
+	color: "medium blue",
+	passengers: 2,
+	convertible: false,
+	mileage: 88000
+}
+
+var chevy = {
+	make: "Chevy",
+	model: "Bel Air",
+	year: 1957, 
+	color: "red",
+	passengers: 2,
+	convertible: false,
+	mileage: 1021
+}
+
 function prequal(car) {
 	if (car.mileage > 10000) {
 		return false;
@@ -17,6 +47,10 @@ function prequal(car) {
 	}
 	return true;
 }
+
+prequal(cadi);
+prequal(fiat);
+prequal(chevy);
 
 var worthALook = prequal(taxi);
 
---
+++ secretcode.js
@@ -0,0 +1,5 @@
+var access = 
+	document.getElementById("code 9");
+var code = access.innerHTML;
+	code = code + " midnight";
+alert(code);
