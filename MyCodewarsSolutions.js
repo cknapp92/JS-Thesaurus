3dbdd86466605856805b3a1ffe9ba926e96a369f
Conrad Knapp <conrad.knapp@bc.edu>
2017-08-27 16:10:08 -0400

    solution: fake binary

--- MyCodewarsSolutions.js
+++ MyCodewarsSolutions.js
@@ -427,3 +427,10 @@ function doubleChar(str) {
 
 doubleChar('something'); // "ssoommeetthhiinngg"
 
+
+
+/* Fake binary */
+
+function fakeBin(x){
+	return x.split('').map(i => i < 5 ? '0' : '1').join('');
+}
