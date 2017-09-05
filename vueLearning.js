bd627e836f9c12fd1d9ed8f6d83f49efd0dfd24c
Conrad Knapp <conrad.knapp@bc.edu>
2017-09-05 01:15:19 -0400

    first commit

---
+++ vueLearning.js
@@ -0,0 +1,93 @@
+// Button click counter
+
+https://jsfiddle.net/cknapp92/ybtg62ay/
+
+
+// Listening for event
+
+https://jsfiddle.net/cknapp92/ybtg62ay/2/
+
+
+
+// Pass event argument to button counter
+
+https://jsfiddle.net/cknapp92/qp57jhxa/
+
+
+
+
+// Stop propagation / .stop / .prevent
+
+https://jsfiddle.net/cknapp92/n1p0g1s8/
+
+/* Can use event.stopPropagation() in a separate method or stop (see mousemove.stop). 
+Stops propagation of the click event. Can also chain .stop with .prevent */
+
+
+// Add key modifiers
+
+https://jsfiddle.net/cknapp92/f3qu6g8h/
+
+// Sends alert only when enter is pressed. Use key-up function, connected with enter keyword
+// to call a method when a given key is pressed
+
+
+
+// Vanilla JS in Vue
+
+https://jsfiddle.net/cknapp92/c4f20kjr/
+
+
+
+// Two-Way Binding
+
+
+https://jsfiddle.net/cknapp92/dbnfrym7/
+
+
+// Watch object
+
+https://jsfiddle.net/cknapp92/mn22trsy/
+
+// Watch object performs a method on a given property in this case counter
+
+
+
+
+// Add/remove colors to blocks on click
+
+https://jsfiddle.net/cknapp92/ygu667v3/
+
+
+
+// Computed property to change box colors
+
+https://jsfiddle.net/cknapp92/j59q66py/
+
+
+
+// Change bg color of blocks using input
+
+
+https://jsfiddle.net/cknapp92/z6hkdru5/
+
+
+
+
+// Directly change the bg color through input w/o css classes
+
+
+https://jsfiddle.net/cknapp92/fj59d2rz/
+
+
+
+// Hide element using v-if / v-else
+
+
+https://jsfiddle.net/cknapp92/co9cgwkv/
+
+
+
+// Iterate over array with v-for
+
+https://jsfiddle.net/cknapp92/j7yzubrz/
