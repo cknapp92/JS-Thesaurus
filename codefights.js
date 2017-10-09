28ac0aba761789aca99b6b950e049d5d07bda14c
Conrad Knapp <conrad.knapp@bc.edu>
2017-10-08 20:36:13 -0400

    solve challenge first duplicate

--- codefights.js
+++ codefights.js
@@ -1,16 +1,34 @@
 /*
 
-Arrays - firstDuplicate
+Arrays - firstDuplicate (attempt 1)
 
 function firstDuplicate(a) {
 
-for (var i = 0; i < a.length; i++) {
-    if (a.indexOf(a[i]) !== i) {
-      return a[i];
-    }
+for (let i = 0; i < a.length; i++) {
+  if (a.indexOf(a[i]) !== i) {
+    return a[i];
+  }
+}
+  return -1;
+}
+
+
+(attempt 2)
+
+function firstDuplicate(a) {
+
+var hash = {};
+
+for (let i = 0; i < a.length; i++) {
+  if(hash[a[i]]) {
+    return a[i];
+  }
+    hash[a[i]] = true;
   }
   return -1;
 }
+
+
 
 Strings - Amend the Sentence
 
