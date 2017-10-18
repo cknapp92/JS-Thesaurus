31bc0632879804a063025d21dd9fb0d325f43c76
Conrad Knapp <conrad.knapp@bc.edu>
2017-10-17 20:14:50 -0400

    solutions: merge sort, bubble sort

--- codefights.js
+++ codefights.js
@@ -89,4 +89,84 @@ function isSentenceCorrect(sentence) {
   return re.test(sentence);
 }
 
+
+Bubble Sort
+
+function bubbleSort(items) {
+
+  var swap = function(firstIndex, secondIndex){
+      var temp = items[firstIndex];
+      items[firstIndex] = items[secondIndex];
+      items[secondIndex] = temp;
+  }
+
+  var len =  items.length ,
+      i, j, stop;
+
+  for (i = 0; i < len; i++){
+      for (j = 0, stop = len - i; j < stop - 1; j++){
+          if (items[j] > items[j + 1]){
+              swap(j, j + 1);
+          }
+      }
+  }
+
+  return items;
+}
+
+
+Merge Sort
+
+
+function mergeSort(sequence) {
+  var merge = function(sequence, left, middle, right) {
+
+    var result = [];
+    var i;
+    var j;
+
+    for (i = left, j = middle; i < middle && j < right; ) {
+      if (sequence[i] < sequence[j]) {
+        result.push(sequence[i]);
+        i++;
+      }
+      else {
+        result.push(sequence[j]);
+        j++;
+      }
+    }
+
+    while (i < middle) {
+      result.push(sequence[i]);
+      i++;
+    }
+
+    while (j < right) {
+      result.push(sequence[j]);
+      j++;
+    }
+
+    for (i = left; i < right; i++) {
+      sequence[i] = result[i - left];
+    }
+  }
+
+  var split = function(sequence, left, right) {
+
+    var middle = Math.floor((left + right) / 2);
+    if (left + 1 === right) {
+            return;
+        }
+
+    split(sequence, left, middle);
+    split(sequence, middle, right);
+    merge(sequence, left, middle, right);
+  }
+
+  split(sequence, 0, sequence.length);
+
+  return sequence;
+}
+
+
 */
